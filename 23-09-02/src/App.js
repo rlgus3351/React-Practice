b6a865adfd20bfacb46e80d01150ea80143b5641
rlgus3351@naver.com <rlgus3351@naver.com>
2023-09-03 17:33:12 +0900

    23/09/03

--- 23-09-02/src/App.js
+++ 23-09-02/src/App.js
@@ -1,6 +1,6 @@
 import MyComponent from "./MyComponent";
 const App = () =>{
-  return <MyComponent name="React"/>
+  return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>
 }
 
 export default App;
