2cf70cfba385618f5434eea609d49582421df069
권기현 <rlgus3351@naver.com>
2023-09-08 15:04:01 +0900

    23/09/08

--- 23-09-02/src/Say.js
+++ 23-09-02/src/Say.js
@@ -13,7 +13,6 @@ const Say = () => {
             <button style={{color:'red'}} onClick={()=> setColor('red')}>빨간색</button>
             <button style={{color:'green'}} onClick={()=> setColor('green')}>초록색</button>
             <button style={{color:'blue'}} onClick={()=> setColor('blue')}>파란색</button>
-            <button style={{color:'aqua'}} onClick={()=> setColor('aqua')}>파란색</button>
         </div>
         
     )
