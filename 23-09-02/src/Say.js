434996fc628bbd1b30191877a32515f67fcb49e1
rlgus3351@naver.com <rlgus3351@naver.com>
2023-09-10 13:06:22 +0900

    23/09/10

--- 23-09-02/src/Say.js
+++ 23-09-02/src/Say.js
@@ -13,6 +13,7 @@ const Say = () => {
             <button style={{color:'red'}} onClick={()=> setColor('red')}>빨간색</button>
             <button style={{color:'green'}} onClick={()=> setColor('green')}>초록색</button>
             <button style={{color:'blue'}} onClick={()=> setColor('blue')}>파란색</button>
+            <button style={{color:'blue'}} onClick={()=> setColor('blue')}>파란색</button>
         </div>
         
     )
