95c4e419e4a8a67c8f4dc15bb6d315b7e02f4736
rlgus3351@naver.com <rlgus3351@naver.com>
2023-09-10 22:28:32 +0900

    23/09/10

--- 23-09-02/src/EventPractice.js
+++ 23-09-02/src/EventPractice.js
@@ -1,47 +1,41 @@
-import { Component } from "react";
+import { useState } from "react";
 
-class EventPractice extends Component{
-    state = {
-        username:'',
-        message: ''
-    }
+const EventPractice = () =>{
+    const[username, setUserName] = useState('');
+    const[message, setMessage] = useState('');
+    const onChangeUserName = e => setUserName(e.target.value);
+    const onChangeMessage = e => setMessage(e.target.value);
+    const onClick=() =>{
+        alert(username +" :" + message);
+        setUserName('');
+        setMessage('');
+    };
+    const onKeyPress = e =>{
+        if(e.key ==='Enter'){
+            onClick();
+        }
+    };
+    return (
+        <div>
+            <h1>이벤트 연습</h1>
+            <input
+                type='text'
+                name="username"
+                placeholder="사용자 명"
+                value = {username}
+                onChange={onChangeUserName}
+            />
+            <input
+                type='text'
+                name="message"
+                placeholder="아무거나 입력해 보세요"
+                value = {message}
+                onChange={onChangeMessage}
+                onKeyPress={onKeyPress}
+            />
+            <button onClick={onClick}>확인</button>
+        </div>
+    );
+};
 
-    handleChange = (e) =>{
-        this.setState({
-            [e.target.name] : e.target.value
-        });
-    }
-
-    handleClick = () => {
-        alert(this.state.username+": "+this.state.message);
-        this.setState({
-            username:'',
-            message:''
-        });
-    }
-
-    render(){
-        return (
-            <div>
-                <h1>이벤트 연습</h1>
-                <input
-                    type="text"
-                    name="username"
-                    placeholder="사용자 명"
-                    value={this.state.username}
-                    onChange={this.handleChange}
-                />
-                <input
-                    type="text"
-                    name="message"
-                    placeholder="아무거나 입력해 보세요"
-                    value={this.state.message}
-                    onChange={this.handleChange}
-                />
-                <button onClick={this.handleClick}
-            >확인</button>
-            </div>
-        )
-    }
-}
 export default EventPractice;
