33a7bc8df68e2fa68151f2d5c3ea86689ab204d3
권기현 <rlgus3351@naver.com>
2023-09-15 14:35:33 +0900

    23/09/15

--- 23-09-02/src/IterationSample.js
+++ 23-09-02/src/IterationSample.js
@@ -1,11 +1,40 @@
+import { useState } from "react";
+
 const IterationSample = () =>{
-    return(
-        <ul>
-            <li>눈사람</li>
-            <li>얼음</li>
-            <li>눈</li>
-            <li>바람</li>
-        </ul>
+    const [names, setNames] = useState([
+        { id: 1, text:'눈사람'},
+        { id: 2, text:'얼음'},
+        { id: 3, text:'눈'},
+        { id: 4, text:'바람'},
+    ]);
+    
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5);
+
+    const onChange = e => setInputText(e.target.value);
+
+    const nameList = names.map(name => <li key={name.id} onDoubleClick={() => onRemove(name.id)}>{name.text}</li>);
+
+    const onRemove = id =>{
+        const nextNames = names.filter(name => name.id !== id);
+        setNames(nextNames);
+    };
+    const onClick = () =>{
+        const nextNames = names.concat({
+            id:nextId,
+            text:inputText
+        });
+        setNextId(nextId+1);
+        setNames(nextNames);
+        setInputText('');
+    }
+
+    return (
+        <>
+            <input value={inputText} onChange={onChange}/>
+            <button onClick={onClick}>추가</button>
+            <ul>{nameList}</ul>
+        </>
     )
 }
 
---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,8 @@
+import Counter from './Counter';
+
+const App = () =>{
+  return <Counter/>
+
+}
+
+export default App;
