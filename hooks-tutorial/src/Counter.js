3fac280028aeb6282a73f7b8d860d6a11eb93f10
rlgus3351@naver.com <rlgus3351@naver.com>
2023-09-16 18:30:30 +0900

    23/09/16

--- hooks-tutorial/src/Counter.js
+++ hooks-tutorial/src/Counter.js
@@ -8,8 +8,8 @@ const Counter = () =>{
             <p>
                 현재 카운터 값은 <b>{value}</b>입니다.
             </p>
-            <button onClick={()=> setValue(value+1)}>+1</button>
-            <button onClick={()=> setValue(value-1)}>-1</button>
+            <button onClick={()=> setValue(value*2)}>*2</button>
+            <button onClick={()=> setValue(value/2)}>%2</button>
         </div>
     )
 }
